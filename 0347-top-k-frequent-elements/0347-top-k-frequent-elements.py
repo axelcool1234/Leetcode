08a0a49cb71f3adaf0e64d92a0db2cc5bad569eb
Axelsoft <63621709+axelcool1234@users.noreply.github.com>
2023-03-02 15:43:05 -0800

    Time: 120 ms (31.62%), Space: 19.6 MB (25.19%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,7 +1,21 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         num_dict = dict()
-        for i in nums:
-            num_dict[i] = num_dict.get(i, 0) + 1
-        num_dict = sorted(num_dict, key=lambda x:num_dict.get(x))
-        return [num_dict[-i] for i in range(1,k+1)]
+        #Index is how much a number appears in nums, sublist contains numbers of that count index.
+        num_frequency = [[] for i in range(len(nums) + 1)]
+        #O(n) time, cycles through nums list and adds it to a hash map ditionary, increasing the
+        #count of any repeated numbers.
+        for num in nums:
+            num_dict[num] = num_dict.get(num, 0) + 1
+        #O(n) time, cycles through hashmap and adds all keys (numbers) to a list, with its count
+        #being the index of the sublist it'll be contained in.
+        for num, count in num_dict.items():
+            num_frequency[count].append(num)
+        #O(n) time, cycles through num_frequency backwards, adding all numbers with the highest count
+        #(which is why we cycle backwards, since higher counts have higher indices).
+        return_list = []
+        for count in range(len(num_frequency) - 1, 0, -1):
+            for num in num_frequency[count]:
+                return_list.append(num)
+                if len(return_list) == k:
+                    return return_list
